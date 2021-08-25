4af804c92b860c7bb010657a70a1a6db0474bbb8
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-08-25 15:40:19 -0700

    cat image and audio

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 // reference needed DOM elements
 
+const catImage = document.getElementById('cat-image')
+
 
 // set event listeners 
+const catAudio = document.getElementById('cat-audio')
+console.log(catAudio);
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+catImage.addEventListener('click', () => {
+catAudio.play();
+
+});
